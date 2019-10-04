caa4fd82896aeac729186d07728a84b1b7843630
Coxik <coxik.coxik@gmail.com>
2019-10-04 20:38:38 +0300

    If-else

---
+++ script.js
@@ -0,0 +1,9 @@
+var age = 28;
+if (age >= 25) {
+    message = "I am over 25 years old";
+}
+else {
+    message = "I am under 25";
+}
+
+var result = ( age >= 25 ) ? "I am over 25 years old" : "I am under 25";
